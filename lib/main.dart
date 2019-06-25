26e89e820a4340513587199e971cacd90625863b
unknown <menasamercap@gmail.com>
2019-06-25 09:37:35 +0200

    changed from stateless to stateful

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp()); // one line function only ...
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Teaster'];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,18 +25,28 @@ class MyApp extends StatelessWidget {
                 Container(
                   margin: EdgeInsets.all(10.0),
                   child: RaisedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                       _products.add('Adbanded Taster'); 
+                      });
+                    },
                     child: Text('Add Product'),
                   ),
                 ),
-                Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/food.jpg'),
-                      Text('Food Paradise')
-                    ],
-                  ),
-                ),
+                Column(
+                  children: _products
+                      .map(
+                        (element) => Card(
+                              child: Column(
+                                children: <Widget>[
+                                  Image.asset('assets/food.jpg'),
+                                  Text(element)
+                                ],
+                              ),
+                            ),
+                      )
+                      .toList(),
+                )
               ],
             )));
   }
