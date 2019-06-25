b3a94dfc1d90cd074db8d95818ede00acbf856ba
Mina Samir <MenaSamerCap@gmail.com>
2019-06-25 14:47:51 +0200

    understood lifecycle

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -2,13 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter_course_2/products.dart';
 
 class ProductManager extends StatefulWidget {
-
   final String startingPorduct;
 
-  ProductManager(this.startingPorduct);
+  ProductManager(this.startingPorduct) {
+    print('[ProductManager] Constructor');
+  }
 
   @override
   State<StatefulWidget> createState() {
+    print('[ProductManager] createState()');
     return _ProductManagerState();
   }
 }
@@ -18,20 +20,36 @@ class _ProductManagerState extends State<ProductManager> {
 
   @override
   void initState() {
-    
+
+    print('[_ProductManagerState] initState()');
+    /// we use widget property to access anything from the StatefulWidget
+    /// that this state belongs to ...
     _products.add(widget.startingPorduct);
 
     super.initState();
+
+    /// we did not need to call setState in initState because
+    /// initState is called before the build method called so
+    /// the data already got their values ..
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+     print('[_ProductManagerState] didUpdateWidget()');
+     print('[_ProductManagerState] didUpdateWidget() ${_products.length}');
+    super.didUpdateWidget(oldWidget);
   }
 
   @override
   Widget build(BuildContext context) {
+    print('[_ProductManagerState] build()');
     return Column(children: <Widget>[
       Container(
         margin: EdgeInsets.all(10.0),
         child: RaisedButton(
           onPressed: () {
             setState(() {
+              /// setState will only call build method and will not reConstruct the all widget
               _products.add('Adbanded Taster');
             });
           },
--- lib/products.dart
+++ lib/products.dart
@@ -3,23 +3,25 @@ import 'package:flutter/material.dart';
 class Products extends StatelessWidget {
   final List<String> _products;
 
-  Products(this._products);
+  Products(this._products) {
+    print('[Product Widget] constructor');
+  }
 
   @override
   Widget build(BuildContext context) {
+    print('[Product Widget] build() ${_products.length}');
     return Column(
-      children: _products
-          .map(
-            (element) => Card(
-                  child: Column(
-                    children: <Widget>[
-                      Image.asset('assets/food.jpg'),
-                      Text(element)
-                    ],
-                  ),
-                ),
-          )
-          .toList(),
+      children: _products.map((element) {
+        print('[Product Widget] in map');
+        return Card(
+          child: Column(
+            children: <Widget>[
+              //Image.asset('assets/food.jpg'),
+              Text(element)
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
