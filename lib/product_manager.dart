69af1dbc52c58b72a9ef45476311256c8fc3f987
Mina Samir <MenaSamerCap@gmail.com>
2019-06-25 12:22:26 +0200

    played with state life cycle

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_course_2/products.dart';
 
 class ProductManager extends StatefulWidget {
+
+  final String startingPorduct;
+
+  ProductManager(this.startingPorduct);
+
   @override
   State<StatefulWidget> createState() {
     return _ProductManagerState();
@@ -9,7 +14,15 @@ class ProductManager extends StatefulWidget {
 }
 
 class _ProductManagerState extends State<ProductManager> {
-  List<String> _products = ['Food Teaster 3'];
+  List<String> _products = [];
+
+  @override
+  void initState() {
+    
+    _products.add(widget.startingPorduct);
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
