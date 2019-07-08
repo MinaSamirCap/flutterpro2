da0c772bae3151e55076a57897c42bff4e87efc5
Mina Samir <MenaSamerCap@gmail.com>
2019-07-08 10:38:42 +0200

    started updated course

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ void main() {
   //debugPaintPointersEnabled = true;
   runApp(MyApp()); // one line function only ...
 
-
   //////////////// the new updated course start from lesson 2
 }
 
@@ -19,11 +18,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-      //debugShowMaterialGrid: true,
-=======
         //debugShowMaterialGrid: true,
->>>>>>> 6c455ac1ba1650f3a8651cd35b4f473a879000db
+
         theme:
             ThemeData(brightness: Brightness.dark, primaryColor: Colors.amber),
         home: HomePage());
