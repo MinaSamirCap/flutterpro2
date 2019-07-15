93007aad453a923d08f1818a9f14b6abbba81220
Mina Samir <MenaSamerCap@gmail.com>
2019-07-15 11:41:01 +0200

    added textField inputs and dateformat library

--- lib/updatedcourse/app1_espenses/app3.dart
+++ lib/updatedcourse/app1_espenses/app3.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course_2/updatedcourse/app1_espenses/transaction.dart';
+import 'package:intl/intl.dart';
 
 class MyApp3 extends StatelessWidget {
   final List<Transaction> transactions = [
@@ -8,6 +9,12 @@ class MyApp3 extends StatelessWidget {
     Transaction(
         id: 't2', title: 'Weekly grocery', amount: 129, date: DateTime.now())
   ];
+
+  String titleInput;
+  String amountInput;
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -23,6 +30,37 @@ class MyApp3 extends StatelessWidget {
             child: Card(
               color: Colors.blueGrey,
               child: Text('CHART'),
+            ),
+          ),
+          Card(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                    controller: titleController,
+                    // onChanged: (value) {
+                    //   titleInput = value;
+                    // },
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amout'),
+                    controller: amountController,
+                    //onChanged: (value) => amountInput = value,
+                  ),
+                  FlatButton(
+                    child: Text('Add Transaction'),
+                    textColor: Colors.purple,
+                    onPressed: () {
+                      print(titleController.value);
+                      //print(titleInput);
+                      //print(amountInput);
+                    },
+                  )
+                ],
+              ),
             ),
           ),
           Column(
@@ -46,8 +84,11 @@ class MyApp3 extends StatelessWidget {
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(item.title, style: TextStyle(fontWeight: FontWeight.bold),),
-                      Text(item.date.toString())
+                      Text(
+                        item.title,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(DateFormat.yMMMMEEEEd().format(item.date))
                     ],
                   )
                 ],
