5258d1b203c12236a45dca9998a224c5c1bd0b03
Mina Samir <MenaSamerCap@gmail.com>
2019-07-16 14:33:13 +0200

    played with theme and fonts amazing vidro

--- lib/updatedcourse/app1_espenses/app3.dart
+++ lib/updatedcourse/app1_espenses/app3.dart
@@ -8,8 +8,22 @@ class MyApp3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.purple, accentColor: Colors.blue),
-      home: HomePageApp3());
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.blue,
+            fontFamily: 'Quicksand',
+            textTheme: ThemeData.light().textTheme.copyWith(
+                title: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold)),
+            appBarTheme: AppBarTheme(
+                textTheme: ThemeData.light().textTheme.copyWith(
+                    title: TextStyle(
+                        fontFamily: 'OpenSans',
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold)))),
+        home: HomePageApp3());
   }
 }
 
@@ -58,7 +72,9 @@ class _HomePageApp3State extends State<HomePageApp3> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ExpensesApp'),
+        title: Text(
+          'Expenses App',
+        ),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.add),
--- lib/updatedcourse/app1_espenses/widgets/transaction_list.dart
+++ lib/updatedcourse/app1_espenses/widgets/transaction_list.dart
@@ -21,8 +21,7 @@ class TransactionList extends StatelessWidget {
               Container(
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
                 decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Theme.of(context).primaryColor, width: 2)),
+                    border: Border.all(color: Theme.of(context).primaryColor, width: 2)),
                 child: Text(
                   '\$ ${item.amount.toStringAsFixed(2)}',
                   style: TextStyle(
@@ -37,7 +36,7 @@ class TransactionList extends StatelessWidget {
                 children: <Widget>[
                   Text(
                     item.title,
-                    style: TextStyle(fontWeight: FontWeight.bold),
+                    style: Theme.of(context).textTheme.title,
                   ),
                   Text(DateFormat.yMMMMEEEEd().format(item.date))
                 ],
