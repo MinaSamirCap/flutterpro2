c9f0613f016ae552702145951d2a421c2411dbad
Mina Samir <MenaSamerCap@gmail.com>
2019-07-16 10:53:57 +0200

    wrapped the view in scroll view and the part of list in a scroll view also but with size

--- lib/updatedcourse/app1_espenses/widgets/transaction_list.dart
+++ lib/updatedcourse/app1_espenses/widgets/transaction_list.dart
@@ -10,37 +10,42 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: userTransaction.map((item) {
-        return Card(
-            child: Row(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.brown, width: 2)),
-              child: Text(
-                '\$ ${item.amount}',
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple),
-              ),
-              padding: EdgeInsets.all(10),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Container(
+      height: 350,
+      child: SingleChildScrollView(
+              child: Column(
+          children: userTransaction.map((item) {
+            return Card(
+                child: Row(
               children: <Widget>[
-                Text(
-                  item.title,
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.brown, width: 2)),
+                  child: Text(
+                    '\$ ${item.amount}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple),
+                  ),
+                  padding: EdgeInsets.all(10),
                 ),
-                Text(DateFormat.yMMMMEEEEd().format(item.date))
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      item.title,
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(DateFormat.yMMMMEEEEd().format(item.date))
+                  ],
+                )
               ],
-            )
-          ],
-        ));
-      }).toList(),
+            ));
+          }).toList(),
+        ),
+      ),
     );
   }
 }
