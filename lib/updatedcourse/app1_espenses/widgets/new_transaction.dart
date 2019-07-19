45dbee32a107aa25d378ec29beb0114630e95021
Mina Samir <MenaSamerCap@gmail.com>
2019-07-19 12:47:11 +0200

    added adaptive button as a separated widget to handle both cases

--- lib/updatedcourse/app1_espenses/widgets/new_transaction.dart
+++ lib/updatedcourse/app1_espenses/widgets/new_transaction.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+import 'adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final Function addNewTransaction;
@@ -46,7 +51,11 @@ class _NewTransactionState extends State<NewTransaction> {
     return SingleChildScrollView(
       child: Card(
         child: Container(
-          padding: EdgeInsets.only(left: 10, top: 10, right: 10, bottom: MediaQuery.of(context).viewInsets.bottom + 10),
+          padding: EdgeInsets.only(
+              left: 10,
+              top: 10,
+              right: 10,
+              bottom: MediaQuery.of(context).viewInsets.bottom + 10),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
@@ -76,14 +85,7 @@ class _NewTransactionState extends State<NewTransaction> {
                           ? 'No Date Selected'
                           : DateFormat.yMd().format(_selectedDate)),
                     ),
-                    FlatButton(
-                      child: Text(
-                        'Choose a date',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      textColor: Theme.of(context).primaryColor,
-                      onPressed: _presentDatePicker,
-                    )
+                    AdaptiveFlatButton('Choose Date', _presentDatePicker),
                   ],
                 ),
               ),
