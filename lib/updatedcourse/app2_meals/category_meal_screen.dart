3877554aa522b351f4c7504ef9879762fd86f4c6
Mina Samir <MenaSamerCap@gmail.com>
2019-07-29 13:29:18 +0200

    navigated to another screen

---
+++ lib/updatedcourse/app2_meals/category_meal_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class CategoryMealScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('The Recipes ;)'),
+        ),
+        body: Center(child: Text('The recipes for category!!')));
+  }
+}
