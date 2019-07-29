0ea293a35cbc2016409c4a92862a54a75a5dc34c
Mina Samir <MenaSamerCap@gmail.com>
2019-07-29 19:11:37 +0200

    finished routes

--- lib/updatedcourse/app2_meals/maal_app_entry_point.dart
+++ lib/updatedcourse/app2_meals/maal_app_entry_point.dart
@@ -24,7 +24,17 @@ class MealApp extends StatelessWidget {
       routes: {
         '/': (ctx) => CategoriesScreen(),
         CategoryMealScreen.ROUTE_NAME: (ctx) => CategoryMealScreen(),
-        MealDetailsScreen.ROUTE_NAME: (ctx) => MealDetailsScreen(),
+        //MealDetailsScreen.ROUTE_NAME: (ctx) => MealDetailsScreen(),
+      },
+     /*  onGenerateRoute: (setting) {
+        return MaterialPageRoute(
+            builder: (ctx) => Center(
+                  child: Text('OnGenerateRoute'),
+                ));
+      }, */
+      onUnknownRoute: (setting) {
+        return MaterialPageRoute(
+            builder: (ctx) => Center(child: Text('OnUnknownRoute')));
       },
     );
   }
