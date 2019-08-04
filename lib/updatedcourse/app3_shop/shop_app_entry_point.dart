3c5b203358a7a7418484262fda03d71d02edde1f
unknown <menasamercap@gmail.com>
2019-08-04 22:41:50 +0200

    finished building the the gird shop item

--- lib/updatedcourse/app3_shop/shop_app_entry_point.dart
+++ lib/updatedcourse/app3_shop/shop_app_entry_point.dart
@@ -1,15 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course_2/updatedcourse/app3_shop/screens/products_overview_screen.dart';
 
 class ShopApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Shop App'),
-        ),
-        body: Center(child: Text('MinaSamir'),),
-      ),
-    );
+    return MaterialApp(home: ProductOverviewScreen());
   }
 }
