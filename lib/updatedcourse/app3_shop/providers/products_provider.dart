efaff9f6fbcb9836d425c0ebe2e38fc624fdab0d
Mina Samir <MenaSamerCap@gmail.com>
2019-08-05 12:52:33 +0200

    finished provider connection

--- lib/updatedcourse/app3_shop/providers/products_provider.dart
+++ lib/updatedcourse/app3_shop/providers/products_provider.dart
@@ -4,14 +4,19 @@ import 'package:flutter_course_2/updatedcourse/app3_shop/models/product.dart';
 import '../dummy_data.dart';
 
 class ProductsProvider with ChangeNotifier {
-
   List<Product> _items = dummyProducts;
 
-  List<Product> get items{
+  List<Product> get items {
     return [..._items];
   }
 
-  void addProduct(){
+  Product findById(String id) {
+    return _items.firstWhere((item) {
+      return item.id == id;
+    });
+  }
+
+  void addProduct() {
     //_items.add()
     notifyListeners();
   }
--- lib/updatedcourse/app3_shop/screens/product_details_screen.dart
+++ lib/updatedcourse/app3_shop/screens/product_details_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course_2/updatedcourse/app3_shop/models/product.dart';
+import 'package:flutter_course_2/updatedcourse/app3_shop/providers/products_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailsScreen extends StatelessWidget {
   static const ROUTE_NAME = '/product-details';
@@ -11,9 +13,12 @@ class ProductDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
 
+    Product product =
+        Provider.of<ProductsProvider>(context, listen: false).findById(productId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(productId),
+        title: Text(product.title),
       ),
     );
   }
