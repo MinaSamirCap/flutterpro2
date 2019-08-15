61dc55fa8e56a615821d99c604bd562a539c1374
Mina Samir <MenaSamerCap@gmail.com>
2019-08-15 22:10:25 +0200

    implemented the logout and auto logout

--- lib/updatedcourse/app3_shop/providers/auth_provider.dart
+++ lib/updatedcourse/app3_shop/providers/auth_provider.dart
@@ -2,11 +2,13 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_course_2/updatedcourse/app3_shop/models/http_exception.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:async';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
+  Timer _authTimer;
 
   String get userId {
     return _userId;
@@ -48,6 +50,7 @@ class Auth with ChangeNotifier {
       _expiryDate = DateTime.now()
           .add(Duration(seconds: int.parse(responseData['expiresIn'])));
 
+      _autoLogout();
       notifyListeners();
     } catch (error) {
       throw error;
@@ -60,5 +63,26 @@ class Auth with ChangeNotifier {
 
   Future<void> login(String email, String password) async {
     return _authenticate(email, password, 'signInWithPassword');
+  }
+
+  void logout() {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+
+    if (_authTimer != null) {
+      _authTimer.cancel();
+      _authTimer = null;
+    }
+    notifyListeners();
+  }
+
+  void _autoLogout() {
+    if (_authTimer != null) {
+      _authTimer.cancel();
+    }
+
+    final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
--- lib/updatedcourse/app3_shop/widgets/app_drawer.dart
+++ lib/updatedcourse/app3_shop/widgets/app_drawer.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_course_2/updatedcourse/app3_shop/providers/auth_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter_course_2/updatedcourse/app3_shop/screens/order_screen.dart';
 import 'package:flutter_course_2/updatedcourse/app3_shop/screens/user_product_screen.dart';
 
@@ -38,6 +40,15 @@ class AppDrawer extends StatelessWidget {
             onTap: () {
               Navigator.of(context)
                   .pushReplacementNamed(UserProductScreen.ROUTE_NAME);
+            },
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout'),
+            onTap: () {
+              Navigator.of(context).pop();
+              Provider.of<Auth>(context, listen: false).logout();
             },
           )
         ],
