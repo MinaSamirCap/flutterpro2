682c323422a4541417323ebbc2852f5c41947a81
Mina Samir <MenaSamerCap@gmail.com>
2019-08-16 12:20:54 +0200

    added hero animation -- shared element in android

--- lib/updatedcourse/app3_shop/widgets/product_item.dart
+++ lib/updatedcourse/app3_shop/widgets/product_item.dart
@@ -25,10 +25,14 @@ class ProductItem extends StatelessWidget {
               Navigator.of(context).pushNamed(ProductDetailsScreen.ROUTE_NAME,
                   arguments: product.id);
             },
-            child: FadeInImage(
-              placeholder: AssetImage('assets/images/product-placeholder.png'),
-              image: NetworkImage(product.imageUrl),
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder:
+                    AssetImage('assets/images/product-placeholder.png'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
             )),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
