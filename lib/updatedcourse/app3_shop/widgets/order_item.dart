b0aafc01f424f311945308078d451c914a0c1612
Mina Samir <MenaSamerCap@gmail.com>
2019-08-16 13:46:27 +0200

    animated orders when expand and collapse

--- lib/updatedcourse/app3_shop/widgets/order_item.dart
+++ lib/updatedcourse/app3_shop/widgets/order_item.dart
@@ -13,54 +13,72 @@ class OrderItem extends StatefulWidget {
   _OrderItemState createState() => _OrderItemState();
 }
 
-class _OrderItemState extends State<OrderItem> {
+class _OrderItemState extends State<OrderItem>
+    with SingleTickerProviderStateMixin {
   var _expanded = false;
+
+  AnimationController _controller;
+  Animation<Offset> _slideTransition;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
+    _slideTransition = Tween<Offset>(
+            begin: Offset(0, 0), end: Offset(0,0))
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Card(
       margin: const EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text('\$${widget.order.amount}'),
-            subtitle: Text(
-                DateFormat('dd/MM/yyyy, hh:mm').format(widget.order.dateTime)),
-            trailing: IconButton(
-              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-            ),
+      child: Column(children: <Widget>[
+        ListTile(
+          title: Text('\$${widget.order.amount}'),
+          subtitle: Text(
+              DateFormat('dd/MM/yyyy, hh:mm').format(widget.order.dateTime)),
+          trailing: IconButton(
+            icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+            onPressed: () {
+              setState(() {
+                _expanded = !_expanded;
+              });
+            },
           ),
-          if (_expanded)
-            Container(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-                height: min(widget.order.products.length * 20.0 + 10, 100),
-                child: ListView(
-                  children: widget.order.products
-                      .map((prod) => Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(
-                                prod.title,
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                '${prod.quantity} x, \$${prod.price} ',
-                                style:
-                                    TextStyle(fontSize: 18, color: Colors.grey),
-                              )
-                            ],
-                          ))
-                      .toList(),
-                ))
-        ],
-      ),
+        ),
+        AnimatedContainer(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+            duration: Duration(milliseconds: 300),
+            curve: Curves.easeIn,
+            height: _expanded
+                ? min(widget.order.products.length * 20.0 + 10, 100)
+                : 0,
+            child: SlideTransition(
+              position: _slideTransition,
+              child: ListView(
+                children: widget.order.products
+                    .map((prod) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              prod.title,
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              '${prod.quantity} x, \$${prod.price} ',
+                              style:
+                                  TextStyle(fontSize: 18, color: Colors.grey),
+                            )
+                          ],
+                        ))
+                    .toList(),
+              ),
+            )),
+      ]),
     );
   }
 }
