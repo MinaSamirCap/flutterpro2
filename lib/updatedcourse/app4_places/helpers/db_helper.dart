59007b9c9009046bf1d8fcd169a23350aee5bf61
unknown <menasamercap@gmail.com>
2019-08-18 00:01:58 +0200

    inserted and fetched form and to database

---
+++ lib/updatedcourse/app4_places/helpers/db_helper.dart
@@ -0,0 +1,24 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart' as path;
+
+class DBHelper {
+  static Future<Database> database() async {
+    final dbPath = await getDatabasesPath();
+    return openDatabase(path.join(dbPath, 'places.db'),
+        onCreate: (db, vsersion) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT)');
+    }, version: 1);
+  }
+
+  static Future<void> insert(String tableName, Map<String, Object> data) async {
+    final db = await DBHelper.database();
+    await db.insert(tableName, data,
+        conflictAlgorithm: ConflictAlgorithm.replace);
+  }
+
+  static Future<List<Map<String, dynamic>>> getData(String tableName) async {
+    final db = await DBHelper.database();
+    return db.query(tableName);
+  }
+}
